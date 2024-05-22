3e11f6ca75b8460c1bc3978d43d2fe5c00fe29db
kripas <kripask50@gmail.com>
2024-05-22 21:30:37 +0545

    visiblity icon changed

--- lib/Textfield.dart
+++ lib/Textfield.dart
@@ -51,7 +51,9 @@ class _OurTextfieldState extends State<OurTextfield> {
                       border:OutlineInputBorder(
                         borderRadius: BorderRadius.circular(17),
                       ),
-                      prefixIconColor: Colors.red,
+                      prefixIconColor: Colors.blue,
+
+
                       suffixIcon:IconButton(
                           onPressed: (){
                            setState(() {
@@ -60,8 +62,8 @@ class _OurTextfieldState extends State<OurTextfield> {
                           },
                           icon: Icon(
                                   clicked
-                                  ?Icons.visibility
-                                  :Icons.visibility_off
+                                  ?Icons.visibility_outlined
+                                  :Icons.visibility_off_outlined
 
                           )
                       ),
